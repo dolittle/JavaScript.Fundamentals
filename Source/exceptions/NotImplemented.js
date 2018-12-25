53ef1506799bfa7f2fea162e1160dbd1c1eb59ee
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-25 23:45:36 +0100

    Fixing copyright message

--- Source/exceptions/NotImplemented.js
+++ Source/exceptions/NotImplemented.js
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+ *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import {Exception} from './Exception';
