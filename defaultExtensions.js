4f1625e443c07c8489b34a4480cb556aefa93ca2
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-26 09:08:44 +0100

    Making more in line with how SystemJS plugins are

--- defaultExtensions.js
+++ defaultExtensions.js
@@ -1,10 +1,11 @@
-(function() {
-    var resolve = System.constructor.prototype.resolve;
-    System.constructor.prototype.resolve = function(url, parentUrl) {
+(function (global) {
+    var prototype = System.constructor.prototype;
+    var resolve = prototype.resolve;
+    prototype.resolve = function (url, parentUrl) {
         return Promise.resolve(resolve.call(this, url, parentUrl))
-        .then(function(result) {            
-            if( result.indexOf('.js') < 0 ) result = result+'.js?'+window.runPostfix;
-            return result;
-        })
+            .then(function (result) {
+                if (result.indexOf('.js') < 0) result = result + '.js?' + window.runPostfix;
+                return result;
+            })
     };
-})();
+})(typeof self !== 'undefined' ? self : global);
