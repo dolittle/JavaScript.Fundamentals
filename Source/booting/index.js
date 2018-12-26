641de33a1846a960150f12645c622d1e5656d8ca
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-26 18:09:14 +0100

    Adding index for all things boot

---
+++ Source/booting/index.js
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+export * from './Boot';
+export * from './BootBuilder';
+export * from './Bootloader';
+export * from './BootloaderResult';
+export * from './BootProcedure';
+export * from './BootProcedures';
