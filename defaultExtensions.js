fbd4725963a9b17ff72999dd073e7b3e08281f74
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-26 09:10:43 +0100

    Putting in module

--- defaultExtensions.js
+++ defaultExtensions.js
@@ -2,6 +2,7 @@
     var prototype = System.constructor.prototype;
     var resolve = prototype.resolve;
     prototype.resolve = function (url, parentUrl) {
+        module = {};
         return Promise.resolve(resolve.call(this, url, parentUrl))
             .then(function (result) {
                 if (result.indexOf('.js') < 0) result = result + '.js?' + window.runPostfix;
