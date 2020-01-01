7363790374384affb3979e42d730f562c2560406
Einar Ingebrigtsen <einar@dolittle.com>
2020-01-01 19:07:07 +0100

    JSDoc

--- Source/validation/rules/Email.ts
+++ Source/validation/rules/Email.ts
@@ -4,10 +4,14 @@
 import { ValueRule } from '../index'
 import { IRuleContext, Reason } from '@dolittle/rules';
 
+/**
+ * Represents a {ValueRule} for email - any value must be a valid email
+ */
 export class Email extends ValueRule {
     static InvalidEmail = Reason.create("A62F369F-9C92-4A06-96C3-654AB0E15119", "Invalid Email '{value}'");
     static expression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/;
 
+    /** @inheritdoc */
     evaluate(context: IRuleContext, source: any): void {
         if (this.failIfValueTypeMismatch(context, source, String)) {
             if( !(<String>source).match(Email.expression) ) {
