1f3c8aaa3389f7a7952ee72a5e0ec78517e5917e
Sindre Wilting <sindrewilting@gmail.com>
2020-07-15 13:06:50 +0200

    Add equatable interfaces to rudiments and remove property accessor stuff to types

---
+++ Source/rudiments/IEquatableTo.ts
@@ -0,0 +1,23 @@
+
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { IEquatable } from './index';
+
+/**
+ * Defines a thing that is equatable to something else that is equatable.
+ *
+ * @export
+ * @interface IEquatableTo
+ * @extends {IEquatable}
+ * @template T
+ */
+export interface IEquatableTo<T extends IEquatable | number | string | boolean> extends IEquatable {
+    /**
+     * Check whether this is equal to other.
+     *
+     * @param {T} other
+     * @returns {boolean}
+     */
+    equals(other: T): boolean
+}
--- Source/rudiments/index.ts
+++ Source/rudiments/index.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+export { IEquatable } from './IEquatable';
+export { IEquatableTo } from './IEquatableTo';
 export { Exception } from './Exception';
 export { Guid } from './Guid';
-export { PropertyAccessor } from './PropertyAccessor';
-export { PropertyAccessorDescriptor } from './PropertyAccessorDescriptor';
-export { PropertyPathResolverProxyHandler } from './PropertyPathResolverProxyHandler';
